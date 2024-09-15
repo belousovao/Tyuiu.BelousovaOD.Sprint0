4bcc5d7bbceebc037b44f1b4d768df8049946ef8
Tyuiu.BelousovaOD@outlook.com <bbb_od@mail.ru>
2024-09-15 17:51:57 +0500

    Task1

---
+++ Tyuiu.BelousovaOD.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.BelousovaOD.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Оля";
+            int age = 18;
+            Console.WriteLine("здравсствуйте, меня зовут " + name + " мне " + age + " лет.");
+            Console.ReadKey();
+        }
+    }
+}
